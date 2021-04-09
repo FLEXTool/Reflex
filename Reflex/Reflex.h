af23d78131af255898f1f2bf393e591f4d63a762
Tanner Bennett <tannerbennett@me.com>
2021-04-08 22:31:24 -0500

    Initial Commit

---
+++ Reflex/Reflex.h
@@ -0,0 +1,18 @@
+//
+//  Reflex.h
+//  Reflex
+//
+//  Created by Tanner on 4/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reflex.
+FOUNDATION_EXPORT double ReflexVersionNumber;
+
+//! Project version string for Reflex.
+FOUNDATION_EXPORT const unsigned char ReflexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflex/PublicHeader.h>
+
+
